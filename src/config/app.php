eba6335192210604587b3b6a3b507c6845ad2045
pablo <p.hernandez.m@ug.uchile.cl>
2024-05-08 15:32:01 +0900

    Fixed PhpUnit test configuration

--- src/config/app.php
+++ src/config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use BladeSvg\BladeSvgServiceProvider;
+
 return [
 
     /*
@@ -165,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        BladeSvgServiceProvider::class,
 
         /*
          * Application Service Providers...

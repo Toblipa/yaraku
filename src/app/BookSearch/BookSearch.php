cf5953d20349d6a47c02a4d3667a5070feabbd40
pablo <p.hernandez.m@ug.uchile.cl>
2024-05-09 20:29:07 +0900

    Fix BookSearch service & added getAllResults for not paginated results

--- src/app/BookSearch/BookSearch.php
+++ src/app/BookSearch/BookSearch.php
@@ -6,19 +6,44 @@ use App\Book;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class BookSearch
 {
     private static $ITEMS_PER_PAGE = 5;
 
-    public static function apply(Request $request): LengthAwarePaginator
+
+    /**
+     * Return filtered paginated items
+     *
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    public static function getResults(Request $request): LengthAwarePaginator
+    {
+        return static::apply($request)->paginate(BookSearch::$ITEMS_PER_PAGE);
+    }
+
+    /**
+     * Return all filtered items
+     *
+     * @param Request $request
+     * @return Collection
+     */
+    public static function getAllResults(Request $request): Collection
+    {
+        return static::apply($request)->get();
+    }
+
+
+    private static function apply(Request $request): Builder
     {
         $query = Book::query();
 
         static::applyDecoratorsFromRequest($request, $query);
 
-        return self::getResults($query);
+        return $query;
     }
 
     private static function applyDecoratorsFromRequest(Request $request, Builder $query): void
@@ -40,10 +65,5 @@ class BookSearch
     private static function isValidDecorator($decorator): bool
     {
         return class_exists($decorator);
-    }
-
-    private static function getResults(Builder $query): LengthAwarePaginator
-    {
-        return $query->paginate(BookSearch::$ITEMS_PER_PAGE);
     }
 }
